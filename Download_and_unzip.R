67d0f59391d13cceb128a10d7b10e7a140dcf6d6
Jesús <jesussantiagoramos@users.noreply.github.com>
2014-07-21 21:33:20 +0200

    Update Download_and_unzip.R

--- Download_and_unzip.R
+++ Download_and_unzip.R
@@ -3,14 +3,14 @@
 # Issue:    Download and unzip a dataset collected from the accelerometers from 
 #           the Samsung Galaxy S smartphone. Please read the README.txt file for 
 #           more information.
-# Author:   Jesús Santiago
+# Author:   Jesus Santiago
 # Date:     20/07/2014
 #--------------------------------------------------------------------------------
 
 # PLEASE ONLY USE THIS SCRIPT IF YOU NEED TO DOWNLOAD THE DATA. 
 # IF YOU HAVE ALREADY DOWNLOADED AND UNZIPPED THE DATASET, RUN THE SCRIPT RUN_ANALYSIS.R
 
-# Download dataset
+# Download data set
 fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 download.file(fileUrl, "dataset.zip")
 dateDownloaded <- date()
