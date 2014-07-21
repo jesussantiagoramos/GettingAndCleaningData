0e30be380322950e65fac8144c50a70877016a88
Jesús <jesussantiagoramos@users.noreply.github.com>
2014-07-21 21:37:36 +0200

    Update Download_and_unzip.R

--- Download_and_unzip.R
+++ Download_and_unzip.R
@@ -1,10 +1,10 @@
 #--------------------------------------------------------------------------------
 # Name:     Download_and_unzip.R
-# Issue:    Download and unzip a dataset collected from the accelerometers from 
+# Purpose:  Download and unzip a dataset collected from the accelerometers from 
 #           the Samsung Galaxy S smartphone. Please read the README.md file for 
 #           more information.
 # Author:   Jesus Santiago
-# Date:     20/07/2014
+# Created:  20/07/2014
 #--------------------------------------------------------------------------------
 
 # PLEASE ONLY USE THIS SCRIPT IF YOU NEED TO DOWNLOAD THE DATA. 
